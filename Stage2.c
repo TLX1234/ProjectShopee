5653a264a1cb4c7a5d32c7d7bb1bb89ca337401d
TLX1234 <tlxtlxtlx051107@gmail.com>
2025-09-29 21:34:20 +0800

    Stage 2: Added Full Suggested Items Display

---
+++ Stage2.c
@@ -0,0 +1,15 @@
+    // suggested items display
+    printf("\nSuggested Items.\n");
+    printf("1. Munchy Chocolate\t2. Math Textbook\t3. Xbox Controller\t4. MSI Monitor 144Hz\t5. Jasmine Seeds\n");
+    printf("Ratings\t112\t\tRatings\t50\t\tRatings\t112\t\tRatings\t454\t\tRatings\t1.2k\n");
+    printf("RM 9.90\t\t\tRM 29.00\t\tRM 185.00\t\tRM 369.90\t\tRM 5.99\t\t\t");
+    printf("\n\n6. Razer Keyboard\t7. Pringles 12x Cans\t8. HyperX Headphones\t9. iPhone 13\t\t10. Deluxe Pillow\n");
+    printf("Ratings\t191\t\tRatings\t4.3k\t\tRatings\t72\t\tRatings\t569\t\tRatings\t3.2k\n");
+    printf("RM 138.88\t\tRM 16.90\t\tRM 241.91\t\tRM 1,809.00\t\tRM 29.80\t\t\t\n\n");
+    printf("11. Marshmello\t\t12. Maggi Kari\t\t13. Sour Candy\t\t14. Gaming Mouse\t15. Mechanical Keyboard\n");
+    printf("Ratings\t87\t\tRatings\t1.1k\t\tRatings\t640\t\tRatings\t800\t\tRatings\t240\n");
+    printf("RM 8.99\t\t\tRM 7.00\t\t\tRM 6.50\t\t\tRM 59.90\t\tRM 249.00\n");
+    printf("\n16. Bluetooth Speaker\t17. Cactus\t\t18. Bonsai Tree\t\t19. Orchid\t\t20. Coffee Beans 1kg\n");
+    printf("Ratings\t410\t\tRatings\t220\t\tRatings\t90\t\tRatings\t150\t\tRatings\t560\n");
+    printf("RM 120.00\t\tRM 12.50\t\tRM 99.00\t\tRM 45.00\t\tRM 39.90\n\n");
+
